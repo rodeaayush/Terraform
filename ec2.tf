db0b97658d8529f325bb4ad78e468ea180e927aa
rodeaayush <rodeaayush2@gmail.com>
2025-07-25 19:04:18 +0530

    first commit

---
+++ ec2.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "ap-south-1"  # Change to your desired AWS region
+}
+
+resource "aws_instance" "Terra-instance" {
+  ami           = "ami-0d0ad8bb301edb745" # Replace with a valid AMI for your region
+  instance_type = "t2.micro"
+
+  key_name      = "ubuntukey"  # Replace with your key pair name
+
+  tags = {
+    name = "Ec2-terraform-Instance"
+  }
+
+}
+
